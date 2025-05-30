e35ba7443209a4717a2a99b0be1851f63e1fba6f
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-05-30 17:58:47 +0500

    RA-13-impl-recipe-screen

--- app/src/main/java/com/renegatemaster/recipeapp/MethodAdapter.kt
+++ app/src/main/java/com/renegatemaster/recipeapp/MethodAdapter.kt
@@ -1,5 +1,6 @@
 package com.renegatemaster.recipeapp
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -18,10 +19,11 @@ class MethodAdapter(private val dataSet: List<String>) :
 
     override fun getItemCount(): Int = dataSet.size
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val method = dataSet[position]
         with(holder.binding) {
-            tvItemMethod.text = method
+            tvItemMethod.text = "${position + 1}. $method"
         }
     }
 }
