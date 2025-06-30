b350f7d14e9e82360b493ce9458883375c938d3a
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-06-30 22:06:40 +0500

    RA-32 URLConnection request

--- app/src/main/java/com/renegatemaster/recipeapp/ui/MainActivity.kt
+++ app/src/main/java/com/renegatemaster/recipeapp/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.renegatemaster.recipeapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import com.renegatemaster.recipeapp.R
 import com.renegatemaster.recipeapp.databinding.ActivityMainBinding
+import com.renegatemaster.recipeapp.model.Category
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -20,6 +25,23 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+            Log.i("!!!", "responseCode: ${connection.responseCode}")
+            Log.i("!!!", "responseMessage: ${connection.responseMessage}")
+            val body = Json.decodeFromString<List<Category>>(
+                connection.inputStream.bufferedReader().readText()
+            )
+            Log.i("!!!", "Body: $body")
+        }
+        thread.start()
 
         with(binding) {
             btnCategories.setOnClickListener {
