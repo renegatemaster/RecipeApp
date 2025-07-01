a31981e314ad12da057877101b313ebdc43c3c02
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-07-01 14:46:38 +0500

    RA-33 create thread pool

--- app/src/main/java/com/renegatemaster/recipeapp/model/Ingredient.kt
+++ app/src/main/java/com/renegatemaster/recipeapp/model/Ingredient.kt
@@ -1,10 +1,13 @@
 package com.renegatemaster.recipeapp.model
 
 import android.os.Parcelable
+import androidx.annotation.StyleRes
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Parcelize
-class Ingredient(
+data class Ingredient(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String,
